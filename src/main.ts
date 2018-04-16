053bc125227dadbcac1e1dbfdad481a9ddce22e4
tabelini <guilherme.tabelini@gmail.com>
2018-04-15 21:29:47 -0300

    Api Docs
    feat(api-docs): Added basic api docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle('Platform API')
+        .setDescription('Platform API description')
+        .setVersion('1.0')
+        .addTag('platform')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('/api-docs', app, document);
+    await app.listen(3000);
 }
+
 bootstrap();
